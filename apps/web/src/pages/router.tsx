978e1dc4b72c36bdcc0a0b20b87602274dc8e2fd
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2025-03-17 09:29:57 -0300

    feat(pages): create sign in page

--- apps/web/src/pages/router.tsx
+++ apps/web/src/pages/router.tsx
@@ -1,10 +1,16 @@
 import { Route, Switch } from 'wouter'
 
+import { SignInPage } from './sign-in'
+import { SignUpPage } from './sign-up'
+
 export function Router() {
   return (
     <Switch>
       <Route path="/">
-        <h1>Initial Environment</h1>
+        <SignUpPage />
+      </Route>
+      <Route path="/sign-in">
+        <SignInPage />
       </Route>
     </Switch>
   )
