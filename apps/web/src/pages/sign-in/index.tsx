d3a982aef565d2843e390c00de73818ca070e5af
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2025-03-18 20:10:29 -0300

    feat(web/pages): add interaction to sign-in and sign-up pages

--- apps/web/src/pages/sign-in/index.tsx
+++ apps/web/src/pages/sign-in/index.tsx
@@ -1,8 +1,54 @@
-import { Link } from 'wouter'
+import { AxiosError } from 'axios'
+import { useForm } from 'react-hook-form'
+import { Link, useLocation } from 'wouter'
 
 import { Button, Input } from '@app/components'
+import { useToast } from '@app/components/toast/use-toast'
+
+import { HttpUsersService } from '@app/services/users.service'
+
+export type SignInForm = {
+  password: string
+  username: string
+}
 
 export function SignInPage() {
+  const { handleSubmit, register } = useForm<SignInForm>()
+
+  const [, setLocation] = useLocation()
+
+  const { toast } = useToast()
+
+  async function handleSubmitSignInForm(formData: SignInForm) {
+    const userService = HttpUsersService.create()
+
+    try {
+      const { data } = await userService.auth(formData)
+
+      localStorage.setItem('access_token', data.access_token)
+
+      setLocation('/app')
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        console.error(error)
+
+        if (error.status === 401) {
+          return toast({
+            description: 'As credenciais do usuário estão incorretas.',
+            title: 'Erro ao entrar com o usuário',
+            variant: 'error'
+          })
+        }
+
+        return toast({
+          description: 'Ocorreu um erro ao entrar com o usuário informado.',
+          title: `Erro ao entrar com o usuário (Code: ${error.status})`,
+          variant: 'error'
+        })
+      }
+    }
+  }
+
   return (
     <section className="flex h-screen w-full items-center bg-zinc-950">
       <div className="mx-auto w-full max-w-[484px] space-y-8">
@@ -18,7 +64,10 @@ export function SignInPage() {
           </span>
         </header>
 
-        <form className="space-y-4">
+        <form
+          className="space-y-4"
+          onSubmit={handleSubmit(handleSubmitSignInForm)}
+        >
           <div className="space-y-2">
             <label
               className="block text-sm font-medium text-zinc-400"
@@ -26,7 +75,7 @@ export function SignInPage() {
             >
               Nome de usuário
             </label>
-            <Input placeholder="John Doe" />
+            <Input placeholder="John Doe" {...register('username')} />
           </div>
 
           <div className="space-y-2">
@@ -36,7 +85,11 @@ export function SignInPage() {
             >
               Senha
             </label>
-            <Input type="password" placeholder="••••••••" />
+            <Input
+              type="password"
+              placeholder="••••••••"
+              {...register('password')}
+            />
           </div>
 
           <Button type="submit" className="mt-8 w-full font-medium">
--- apps/web/src/pages/sign-up/index.tsx
+++ apps/web/src/pages/sign-up/index.tsx
@@ -1,8 +1,60 @@
-import { Link } from 'wouter'
+import { AxiosError } from 'axios'
+import { useForm } from 'react-hook-form'
+import { Link, useLocation } from 'wouter'
 
 import { Button, Input } from '@app/components'
+import { useToast } from '@app/components/toast/use-toast'
+
+import { HttpUsersService } from '@app/services/users.service'
+
+export type SignUpForm = {
+  confirmPassword: string
+  password: string
+  username: string
+}
 
 export function SignUpPage() {
+  const { handleSubmit, register } = useForm<SignUpForm>()
+
+  const [, setLocation] = useLocation()
+
+  const { toast } = useToast()
+
+  async function handleSubmitSignUpForm(formData: SignUpForm) {
+    const userService = HttpUsersService.create()
+
+    try {
+      await userService.create(formData)
+
+      const { data } = await userService.auth({
+        password: formData.password,
+        username: formData.username
+      })
+
+      localStorage.setItem('access_token', data.access_token)
+
+      setLocation('/app')
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        console.error(error)
+
+        if (error.status === 401) {
+          return toast({
+            description: 'As senhas informadas não coincidem.',
+            title: 'Erro ao cadastrar usuário',
+            variant: 'error'
+          })
+        }
+
+        return toast({
+          description: 'Ocorreu um erro ao cadastrar o usuário.',
+          title: `Erro ao cadastrar usuário (Code: ${error.status})`,
+          variant: 'error'
+        })
+      }
+    }
+  }
+
   return (
     <section className="flex h-screen w-full items-center bg-zinc-950">
       <div className="mx-auto w-full max-w-[484px] space-y-8">
@@ -18,7 +70,10 @@ export function SignUpPage() {
           </span>
         </header>
 
-        <form className="space-y-4">
+        <form
+          className="space-y-4"
+          onSubmit={handleSubmit(handleSubmitSignUpForm)}
+        >
           <div className="space-y-2">
             <label
               className="block text-sm font-medium text-zinc-400"
@@ -26,7 +81,7 @@ export function SignUpPage() {
             >
               Nome de usuário
             </label>
-            <Input placeholder="John Doe" />
+            <Input placeholder="John Doe" {...register('username')} />
           </div>
 
           <div className="space-y-2">
@@ -36,7 +91,11 @@ export function SignUpPage() {
             >
               Senha
             </label>
-            <Input type="password" placeholder="••••••••" />
+            <Input
+              type="password"
+              placeholder="••••••••"
+              {...register('password')}
+            />
           </div>
 
           <div className="space-y-2">
@@ -46,7 +105,11 @@ export function SignUpPage() {
             >
               Confirme sua senha
             </label>
-            <Input type="password" placeholder="••••••••" />
+            <Input
+              type="password"
+              placeholder="••••••••"
+              {...register('confirmPassword')}
+            />
           </div>
 
           <Button type="submit" className="mt-8 w-full font-medium">
