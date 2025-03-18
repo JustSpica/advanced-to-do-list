456a67c706b7a8310537ecb8a94c2b8652b3e0c7
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2025-03-18 15:02:34 -0300

    feat(domain/use-cases): create fetch tasks use-case

---
+++ apps/api/src/domain/use-cases/fetch-tasks-use-case.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common'
+import { Task } from '@prisma/client'
+
+import { UserDoesNotExistError } from './errors'
+
+import { UsersRepository } from '../repositories/users-repository'
+import { TasksRepository } from '../repositories/tasks-repository'
+
+interface FetchTasksUseCaseRequest {
+  authorId: string
+  page: number
+}
+
+interface FetchTasksUseCaseResponse {
+  tasks: Task[]
+}
+
+@Injectable()
+export class FetchTasksUseCase {
+  constructor(
+    private taskRepository: TasksRepository,
+    private userRepository: UsersRepository
+  ) {}
+
+  async execute({
+    authorId,
+    page
+  }: FetchTasksUseCaseRequest): Promise<FetchTasksUseCaseResponse> {
+    const user = await this.userRepository.findById(authorId)
+
+    if (!user) {
+      throw new UserDoesNotExistError()
+    }
+
+    const tasks = await this.taskRepository.findManyByUserId(authorId, page)
+
+    return {
+      tasks
+    }
+  }
+}
